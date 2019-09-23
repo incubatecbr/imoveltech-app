787c834668141d51457f7fa9418e349168bad832
Igor Raphael <igorraphaeel@live.com>
2019-09-23 15:52:01 -0300

    Init commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Fragment} from 'react';
+import {
+
+  View,
+  Text,
+
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text> ImovelTech </Text>
+    </View>
+  );
+};
+
+export default App;
