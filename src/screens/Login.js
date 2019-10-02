ac05deb22ea3aa66dccff2ef8ddd9eccade8b72f
Igor Raphael <igorraphaeel@live.com>
2019-10-02 15:37:11 -0300

    add logo and validation user

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Database from '../Database';
+const db = new Database();
 
 import {
   StyleSheet,
@@ -15,23 +17,29 @@ export default class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username : 'a',
-      pass: 'a',
-      signedIn: false,
-      checkedSignIn: false,
+      username : '',
+      pass: '',
       errors: 0
     }
   }
-  
+
   onClickListener = (viewId) => {
     Alert.alert("Alert", "Button pressed "+viewId);
   }
 
   onSubmitLoginUser = () => {
     if(this.state.username != '' || this.state.pass != ''){
-      this.props.navigation.navigate('Dashboard')
+      db.selectUser(this.state.username, this.state.pass ).then( (data) => {
+        if(data === false){
+          Alert.alert('Dados incorretos..');
+        }else{
+          this.props.navigation.navigate('Dashboard');
+        }
+      }).catch((err)=>{
+        console.warn(err);
+      });
     }else{
-      Alert.alert('Ops');
+      Alert.alert('Por favor preencha os campos para entrar');
       this.setState({errors : this.state.errors + 1 });
     }
   }
@@ -60,14 +68,14 @@ export default class Login extends Component {
         <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => this.onSubmitLoginUser()}>
           <Text style={styles.loginText}>Login</Text>
         </TouchableHighlight>
-
+{/* 
         <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('recoverypass')}>
             <Text>Esqueceu sua senha?</Text>
         </TouchableHighlight>
 
         <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('register')}>
             <Text>Cadastre-se</Text>
-        </TouchableHighlight>
+        </TouchableHighlight> */}
       </View>
     );
   }
@@ -112,11 +120,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom:20,
-    width:250,
+    width:150,
     borderRadius:30,
   },
   loginButton: {
-    backgroundColor: "#00b5ec",
+    backgroundColor: "#3dbae0",
   },
   loginText: {
     color: 'white',
