1e225d82d9d8101274e10bee15df02ef1c2b5dc5
Igor Raphael <igorraphaeel@live.com>
2019-10-10 17:55:04 -0300

    add lib image picker and convert in base for send back-end

--- src/screens/NewAd.js
+++ src/screens/NewAd.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { SafeAreaView, ScrollView, View, StyleSheet, Text, TextInput, TouchableOpacity, TouchableHighlight, Alert } from 'react-native';
+import { SafeAreaView, ScrollView, View, StyleSheet, Text, TextInput, TouchableOpacity, TouchableHighlight, FlatList, Alert, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Axios from 'axios';
+import ImagePicker from 'react-native-image-picker';
 
 export default class NewAd extends Component {   
     constructor(props) {
@@ -15,6 +16,8 @@ export default class NewAd extends Component {
           sizeRecreation: null,
           pool: null,
           garagem: null,
+          sourceImg: null,
+          base64Img: null,
         };
     }
 
@@ -29,7 +32,10 @@ export default class NewAd extends Component {
         if(this.onValidateData() === true){
             //console.log('Send db');
             //console.log(this.state);
-            const response = await Axios.post("http://192.168.100.154/imoveltech/", {class:'immobile', action:'add', data: this.state});
+            //{class:'immobile', action:'add', data: this.state}
+            //{header: {Authorization: "Bearer access-token", 'Content-Type': 'multipart/form-data'} }
+            //send json form
+            const response = await Axios.post("http://192.168.100.154/imoveltech/",{class:'immobile', action:'add', data: this.state});
             const { data } = response;
             console.log(data);
         }
@@ -49,6 +55,34 @@ export default class NewAd extends Component {
         }else{
             Alert.alert('Por favor preencha todos os campos.');
         } 
+    }
+
+    selectPhotoTapped = () => {
+        const options = {
+          quality: 1.0,
+          maxWidth: 500,
+          maxHeight: 500,
+          mediaType: 'photo',
+          storageOptions: {
+            skipBackup: true,
+          },
+        };
+    
+        ImagePicker.showImagePicker(options, response => {
+          //console.log('Response = ', response);
+          if (response.error) {
+            console.warn('ImagePicker Error: ', response.error);
+          } else {
+            let source = {uri: response.uri};
+            // You can also display the image using data:
+            // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+            this.setState({ 
+                sourceImg: source,
+                base64Img: response.data
+              });
+          }
+
+        });
     }
 
   
@@ -101,12 +135,12 @@ export default class NewAd extends Component {
                 
                     <Text>Possui piscina ?</Text>
                     <View style={styles.radioGroup}>
-                        <TouchableOpacity style={styles.circle} onPress={() => this.setState({ pool: 1 })}> 
-                            { this.state.pool === 1 && (<View style={styles.checkedCircle} />) }
+                        <TouchableOpacity style={styles.circle} onPress={() => this.setState({ pool: '1' })}> 
+                            { this.state.pool === '1' && (<View style={styles.checkedCircle} />) }
                         </TouchableOpacity>
                         <Text style={{ marginHorizontal: 5 }}>Sim</Text>
-                        <TouchableOpacity style={styles.circle} onPress={() => this.setState({ pool: 0 })}> 
-                            { this.state.pool === 0 && (<View style={styles.checkedCircle} />) }
+                        <TouchableOpacity style={styles.circle} onPress={() => this.setState({ pool: '0' })}> 
+                            { this.state.pool === '0' && (<View style={styles.checkedCircle} />) }
                         </TouchableOpacity>
                         <Text style={{ marginHorizontal: 5 }}>Não</Text>
                     </View>
@@ -117,6 +151,20 @@ export default class NewAd extends Component {
                         keyboardType={'numeric'}
                         underlineColorAndroid='transparent'
                         onChangeText={(garagem) => this.setState({garagem})}/>
+
+                    <Text>Clique abaixo para adicionar fotos do imovel</Text>
+                    <TouchableHighlight style={{marginVertical: 5}} onPress={() => this.selectPhotoTapped() }>
+                        <View style={{flexDirection: 'row', padding: 10, borderWidth: 1, borderColor: '#d57900'}}>
+                            <Icon name="plus" size={25} color={'grey'} style={{marginHorizontal:5}}/>
+                            <Icon name="image" size={25} color={'grey'}/>
+                        </View>
+                    </TouchableHighlight>
+                    
+                    {this.state.sourceImg === null  ? (                                                                                                                            
+                        <Text style={{fontSize: 12, color: 'grey'}}>Escolha uma foto..</Text>
+                    ) : (
+                        <Image style={styles.imgPicker} source={this.state.sourceImg} /> 
+                    )}
 
                     <TouchableHighlight style={styles.btn} onPress={() => this.onSubmitAd()}>
                         <Text style={{color: 'white'}}> <Icon name="check-square-o" size={14} color={'white'}/> Anunciar</Text>
@@ -186,5 +234,12 @@ const styles = StyleSheet.create({
         width: '60%',
         borderRadius: 5,
         alignSelf: 'center',
-      },
+    },
+    imgPicker:{
+        marginHorizontal: 2,
+        borderWidth: 1,
+        borderColor: 'black',
+        width: 200,
+        height: 200,
+    },
 });
