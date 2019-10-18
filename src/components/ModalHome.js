fd2b3ee0046ed3d750a32bb4fc5b84274d727112
Igor Raphael <igorraphaeel@live.com>
2019-10-18 16:41:14 -0300

    loading after login

---
+++ src/components/ModalHome.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { View, Modal, StyleSheet } from 'react-native';
+import LottieView from 'lottie-react-native';
+
+
+class ModalHome extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+  render() {
+      
+    return (
+            <Modal
+                animationType="slide"
+                transparent={false}
+                visible={this.props.visible}
+                >
+                <View style={styles.container}>
+                    <LottieView
+                        
+                        source={require('../images/house.json')}
+                        autoPlay loop
+                    />
+                </View>
+            </Modal>
+    );
+  }
+}
+export default ModalHome;
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFF',
+    }
+});
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {ScrollView, SafeAreaView, FlatList, Text, Alert} from 'react-native';
 import Card from '../components/Card';
+import Modal from '../components/ModalHome';
 import Api from '../Api';
 
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { immobiles: [] };
+    this.state = { 
+      immobiles: [],
+      loading: true, 
+    };
   }
 
   async componentDidMount(){
@@ -16,18 +20,36 @@ export default class Home extends Component {
     const { data } = response;
     this.setState({immobiles: data});
     this.props.navigation.setParams({ cControll: 0});//controll for force remount.
+    this.sleepTest(2000).then( () => {
+      this.setState({loading: false});
+    });
   }
+
+  sleepTest = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+  }
+
+
   //Bottom Navigation
   static navigationOptions = {
     tabBarIcon: ({tintColor, activeTintColor}) => (
       <Icon name="home" size={22} color={'#f2611d'}/>
     )
   };
+
+  
   
     render() {
+      // if( this.state.loading ){
+      //   return <Modal/>
+      // }
+
+
       return (
         <SafeAreaView style={{flex:1, backgroundColor:'#FFF'}}>
           <ScrollView>
+           <Modal visible={this.state.loading}/>
+
             <FlatList
               data={this.state.immobiles}
               keyExtractor={item => item.id_}
