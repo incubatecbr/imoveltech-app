ab1940eb6270336056dafc2c896886458ab373b5
Igor Raphael <igorraphaeel@live.com>
2019-10-31 16:04:41 -0300

    last

--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
 export default Axios.create({
-    baseURL: 'http://192.168.100.154/imoveltech/',
+    baseURL: 'http://192.168.100.155/imoveltech/',
     responseType: "json"
 });
